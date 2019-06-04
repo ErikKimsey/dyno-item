d0aa85f0d39cc12c7a803177afef1c6a0764c141
Erik Kimsey <kimseyerik@gmail.com>
2019-06-04 10:00:01 -0400

    First commit

---
+++ src/containers/Dyno.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Dyno extends Component {
+  render() {
+    return (
+      <div>
+        <div className="dyno-container"></div>
+        
+      </div>
+    )
+  }
+}
