9aa902e4a84a90e9b460f6d0e1834258c24d30c7
Erik Kimsey <kimseyerik@gmail.com>
2019-06-14 15:11:46 -0400

    Have a great wave look to the arrows now

--- src/components/scrollArrow.js
+++ src/components/scrollArrow.js
@@ -7,27 +7,29 @@ export default function ScrollArrow() {
 	console.log(cont);
 	// cont.style.display = 'none';
 
-	contI.forEach((e, i) => {
-		i += 1;
-		setTimeout(() => {
-			stagger(e);
-		}, 250 * i);
-		console.log('yo');
-	});
-
-	if (cont !== null) cont.classList.add('hide');
-
 	const stagger = (e) => {
 		e.classList.add('animate-bounce');
 	};
+
+	const hideContainer = () => {
+		if (cont !== null) cont.classList.add('hide');
+	};
+
+	contI.forEach((e, i) => {
+		i += 1;
+		if (i === contI.length - 1) hideContainer();
+		setTimeout(() => {
+			stagger(e);
+			console.log(i * 250);
+		}, 200 * i);
+		console.log('will you wait');
+	});
 
 	return (
 		<div className="scroll-arrow-container">
 			<i className="arrow-1" />
 			<i className="arrow-2" />
 			<i className="arrow-3" />
-			{/* <i className="arrow-2" />
-			<i className="arrow-3" /> */}
 		</div>
 	);
 }
